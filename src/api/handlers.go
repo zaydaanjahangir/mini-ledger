5b87830ce417cdfa4fa9bea69df982e3aeae156e
Zaydaan Jahangir <jahangir.z@northeastern.edu>
2025-10-19 02:48:09 -0400

    feat: Add POST /transactions

--- src/api/handlers.go
+++ src/api/handlers.go
@@ -4,11 +4,22 @@ import (
 	"database/sql"
 	"encoding/json"
 	"net/http"
-	"log"
+	// "log"
+	"fmt"
 )
 
 type Account struct {
     Name string `json:"name"`
+}
+
+type Entry struct {
+	Account string  `json:"account"`
+	Amount  int64 `json:"amount"`
+}
+
+type Transaction struct {
+	Description string `json:"description"`
+	Entries     []Entry `json:"entries"`
 }
 
 type Response struct {
@@ -20,6 +31,7 @@ func StartServer(db *sql.DB) {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/accounts", accountHandler(db))
+	mux.HandleFunc("/transactions", transactionHandler(db))
 
 	http.ListenAndServe(":8080", mux)
 }
@@ -29,7 +41,7 @@ func accountHandler(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request){
 		if req.Method != http.MethodPost {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+			return
 		}
 
 		var account Account
@@ -45,6 +57,7 @@ func accountHandler(db *sql.DB) http.HandlerFunc {
 		}
 
 		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
 		json.NewEncoder(w).Encode(Response{
 			Status:  "success",
 			Message: "Account created successfully",
@@ -53,3 +66,93 @@ func accountHandler(db *sql.DB) http.HandlerFunc {
 
 }
 
+func transactionHandler(db *sql.DB) http.HandlerFunc {
+
+	return func(w http.ResponseWriter, req *http.Request) {
+		if req.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		var transaction Transaction
+		if err := json.NewDecoder(req.Body).Decode(&transaction); err != nil {
+			http.Error(w, "Invalid JSON", http.StatusBadRequest)
+			return
+		}
+
+		if len(transaction.Entries) < 2 {
+			http.Error(w, "Transaction must have at least two entries", http.StatusBadRequest)
+			return
+		}
+
+		tx, err := db.Begin()
+		if err != nil {
+			http.Error(w, "Error starting transaction", http.StatusInternalServerError)
+			return
+		}
+		defer tx.Rollback()
+
+		// TODO: Store each account’s ID in a map so you don’t query twice
+		var total int64
+		for _, entry := range transaction.Entries {
+			var exists bool
+			err := tx.QueryRow("SELECT EXISTS(SELECT 1 FROM accounts WHERE name = ?)", entry.Account).Scan(&exists)
+			if err != nil {
+				http.Error(w, "Database error", http.StatusInternalServerError)
+				return
+			}
+			if !exists {
+				http.Error(w, fmt.Sprintf("Account %s does not exist", entry.Account), http.StatusBadRequest)
+				return
+			}
+
+			total += entry.Amount
+		}
+
+		if total != 0 {
+			http.Error(w, "Entries do not balance (sum must equal 0)", http.StatusBadRequest)
+			return
+		}
+
+		res, err := tx.Exec("INSERT INTO transactions(description) VALUES(?)", transaction.Description)
+		if err != nil{
+			http.Error(w, "Error fetching transaction id", http.StatusInternalServerError)
+			return
+		}
+		transactionID, _ := res.LastInsertId()
+		
+
+		
+		stmt, err := tx.Prepare("INSERT INTO entries(transaction_id, account_id, amount) VALUES(?, ? ,?)")
+		if err != nil {
+			http.Error(w, "Error preparing query", http.StatusInternalServerError)
+		}
+		defer stmt.Close()
+		for _, entry := range transaction.Entries {
+			var accountID int
+			err = tx.QueryRow("SELECT id FROM accounts WHERE name = ?", entry.Account).Scan(&accountID)
+			if err != nil {
+				http.Error(w, "Error finding account id", http.StatusInternalServerError)
+				return
+			}
+			_, err = stmt.Exec(transactionID, accountID, entry.Amount)
+			if err != nil {
+				http.Error(w, "Error writing entries", http.StatusInternalServerError)
+				return
+			}
+		}
+
+		err = tx.Commit()
+		if err != nil {
+			http.Error(w, "Error committing transaction", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusCreated)
+		json.NewEncoder(w).Encode(map[string]string{
+			"status":  "success",
+			"message": "Transaction validated successfully",
+		})
+	}
+}
